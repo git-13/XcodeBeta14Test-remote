f045e629db101e138c0fa7ee05b566c22fc27459
Vladislav <git13vl@gmail.com>
2022-06-25 00:29:25 +0200

    Initial Commit

---
+++ XcodeBeta14Test/XcodeBeta14TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeBeta14TestApp.swift
+//  XcodeBeta14Test
+//
+//  Created by Vladislav Glovatskiy on 25.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct XcodeBeta14TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
